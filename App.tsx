81cfdf318f3c55976db9caaa01f42806103e89da
George Bezerra <george.bezerra@fgv.br>
2022-07-18 11:52:12 -0300

    configurando projeto

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import {SigIn} from './src/screens/Signin';
+
+export default function App() {
+  return (
+      <SigIn />
+  );
+}
+
+ 
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,9 @@
+import { View,Text} from 'react-native';
+
+ export function SigIn(){
+  return (
+   <View>
+      <Text>Ola,George</Text>
+   </View>     
+  );
+}
